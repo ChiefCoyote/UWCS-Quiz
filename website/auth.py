0537cc6dde9b06f23c3a9f9e504cd76f5573e001
Oliver Cordeaux <126358259+ChiefCoyote@users.noreply.github.com>
2024-08-31 00:54:35 +0100

    Added render templates and began work on the authentication system

---
+++ website/auth.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template('login.html')
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    return render_template('sign-up.html')
