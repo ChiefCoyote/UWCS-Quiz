ff6f09359e056855405648b9732553759b74003f
Oliver Cordeaux <126358259+ChiefCoyote@users.noreply.github.com>
2024-09-04 02:30:08 +0100

    Written models for remainder of database as well as creating required html files for necessary webpages
    Database models:
    Can now store questions, rounds, quizzes and their relationships between each other and the user
    Teams and scorers can be created with a score count and will be tracked for each session
    
    Random unique string generator implemented for share codes and session codes (For future could abstract out as it is essentially the same for each except for the check for uniqueness)
    
    HTML files:
    Planned out the user experience for webpage interaction and so created the the files to be written later.

--- website/models.py
+++ website/models.py
@@ -1,9 +1,151 @@
 from . import db
 from flask_login import UserMixin
+import random
+from string import ascii_uppercase
 
 class User(db.Model, UserMixin):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(150), unique=True)
     passwordHash = db.Column(db.String(20))
     isVerified = db.Column(db.Boolean(), default = False)
+
+class Quiz(db.Model):
+    __tablename__ = "quizzes"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    shareCode = db.Column(db.String, unique = True)
+
+    def __init__(self, name):
+        self.name = name
+        self.shareCode = self.generate_share_code()
+        
+    def generate_share_code(self):
+        while True:
+            code = ""
+            for _ in range(4):
+                code += random.choice(ascii_uppercase)
+            if not db.session.query(Quiz).filter_by(shareCode = code).first():
+                return code
+
+class Question(db.Model):
+    __tablename__ = "questions"
+    id = db.Column(db.Integer, primary_key=True)
+    multChoice = db.Column(db.Boolean(), default = False)
+    text = db.Column(db.String(500))
+    media = db.Column(db.String(50), default = None)
+    choices = db.Column(db.String, default = None)
+    answer = db.Column(db.String(255))
+    shareCode = db.Column(db.String, unique = True)
+
+    def __init__(self, multChoice, text, media, answer):
+        self.multChoice = multChoice
+        self.text = text
+        self.media = media
+        self.answer = answer
+        self.shareCode = self.generate_share_code()
+
+    def __init__(self, multChoice, text, answer):
+        self.multChoice = multChoice
+        self.text = text
+        self.answer = answer
+        self.shareCode = self.generate_share_code()
+        
+    def generate_share_code(self):
+        while True:
+            code = ""
+            for _ in range(4):
+                code += random.choice(ascii_uppercase)
+            if not db.session.query(Question).filter_by(shareCode = code).first():
+                return code
+            
+    def set_data(self, data_list):
+        self.choices = '|'.join(data_list)
+
+    def get_data(self):
+        return self.choices.split('|')
+    
+class Round(db.Model):
+    __tablename__ = "rounds"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    shareCode = db.Column(db.String, unique = True)
+
+    def __init__(self, name):
+        self.name = name
+        self.shareCode = self.generate_share_code()
+        
+    def generate_share_code(self):
+        while True:
+            code = ""
+            for _ in range(4):
+                code += random.choice(ascii_uppercase)
+            if not db.session.query(Round).filter_by(shareCode = code).first():
+                return code
+            
+class UserQuiz(db.Model):
+    __tablename__ = "user_quizzes"
+    userID = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    quizID = db.Column(db.Integer, db.ForeignKey('quizzes.id'), primary_key=True)
+
+    user = db.relationship('User')
+    quiz = db.relationship('Quiz')
+
+class QuizRound(db.Model):
+    __tablename__ = "quiz_rounds"
+    quizID = db.Column(db.Integer, db.ForeignKey('quizzes.id'), primary_key=True)
+    roundID = db.Column(db.Integer, db.ForeignKey('rounds.id'), primary_key=True)
+
+    quiz = db.relationship('Quiz')
+    round = db.relationship('Round')
+
+class RoundQuestion(db.Model):
+    __tablename__ = "round_questions"
+    roundID = db.Column(db.Integer, db.ForeignKey('rounds.id'), primary_key=True)
+    questionID = db.Column(db.Integer, db.ForeignKey('questions.id'), primary_key=True)
+
+    round = db.relationship('Round')
+    question = db.relationship('Question')
+
+
+
+class QuizSession(db.Model):
+    __tablename__ = "sessions"
+    sessionID = db.Column(db.Integer, primary_key=True)
+    quizID = db.Column(db.Integer)
+    joinCode = db.Column(db.String, unique = True)
+
+    teams = db.relationship('Team', backref='session', cascade='all, delete-orphan')
+    scorers = db.relationship('Scorer', backref='session', cascade='all, delete-orphan')
+
+    def __init__(self, quizID):
+        self.quizID = quizID
+        self.joinCode = self.generate_join_code()
+        
+    def generate_join_code(self):
+        while True:
+            code = ""
+            for _ in range(4):
+                code += random.choice(ascii_uppercase)
+            if not db.session.query(Quiz).filter_by(joinCode = code).first():
+                return code
+            
+class Team(db.Model):
+    __tablename__ = "teams"
+    teamID = db.Column(db.Integer, primary_key=True)
+    teamName = db.Column(db.String(50), unique = True)
+    sessionID = db.Column(db.Integer, db.ForeignKey('sessions.sessionID'))
+    score = db.Column(db.Integer, default = 0)
+
+    answers = db.relationship('TeamAnswer', backref='team', cascade='all, delete-orphan')
+
+class TeamAnswer(db.Model):
+    __tablename__ = "team_answers"
+    teamID = db.Column(db.Integer, db.ForeignKey("teams.teamID"), primary_key=True)
+    questionID = db.Column(db.Integer, db.ForeignKey("questions.id"), primary_key = True)
+
+class Scorer(db.Model):
+    __tablename__ = "scorers"
+    scorerID = db.Column(db.Integer, primary_key=True)
+    sessionID = db.Column(db.Integer, db.ForeignKey('sessions.sessionID'))
--- website/views.py
+++ website/views.py
@@ -5,3 +5,35 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/join')
+def join():
+    return render_template("join.html")
+
+@views.route('/participant_wait')
+def participant_wait():
+    return render_template("participant_wait.html")
+
+@views.route('/answer')
+def answer():
+    return render_template("answer.html")
+
+@views.route('/quizzes')
+def quizzes():
+    return render_template("quizzes.html")
+
+@views.route('/host_wait')
+def host_wait():
+    return render_template("host_wait.html")
+
+@views.route('/question')
+def question():
+    return render_template("question.html")
+
+@views.route('/mark')
+def mark():
+    return render_template("mark.html")
+
+@views.route('/final_score')
+def final_score():
+    return render_template("final_score.html")
