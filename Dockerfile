b215db8bfde9e9bf3080fe68a58dc9360b8b9317
Oliver Cordeaux <126358259+ChiefCoyote@users.noreply.github.com>
2024-10-26 20:37:25 +0100

    Added eventlet for websocket compatability

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN pip install pipenv
 WORKDIR /UWCS-Quiz
 COPY . /UWCS-Quiz
 RUN pipenv install --system
-CMD gunicorn main:main -b 0.0.0.0:8080
+CMD gunicorn --worker-class eventlet -w 1 main:app
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app, socketio
+import eventlet
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port=8080)
